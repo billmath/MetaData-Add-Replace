5ad612d5d81066918c476c2685317a41749b3193
billmath <billmath@microsoft.com>
2017-12-04 13:00:06 -0500

    Add project files.

---
+++ MetaDataReplacer/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace MetaDataReplacer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDir = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbAttribute = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 25);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Directory:";
+            // 
+            // txtDir
+            // 
+            this.txtDir.Location = new System.Drawing.Point(150, 39);
+            this.txtDir.Name = "txtDir";
+            this.txtDir.Size = new System.Drawing.Size(653, 31);
+            this.txtDir.TabIndex = 8;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(872, 101);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(119, 52);
+            this.btnGo.TabIndex = 7;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(872, 28);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(119, 52);
+            this.btnBrowse.TabIndex = 6;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 25);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Attribute:";
+            // 
+            // cbAttribute
+            // 
+            this.cbAttribute.FormattingEnabled = true;
+            this.cbAttribute.Items.AddRange(new object[] {
+            "ms.author:",
+            "ms.reviewer:",
+            "author:",
+            "manager:",
+            "ms.custom:",
+            "services:",
+            "documentationcenter: ",
+            "editor: ",
+            "ms.service:",
+            "ms.workload:",
+            "ms.tgt_pltfrm:",
+            "ms.devlang:",
+            "ms.topic:"});
+            this.cbAttribute.Location = new System.Drawing.Point(150, 98);
+            this.cbAttribute.Name = "cbAttribute";
+            this.cbAttribute.Size = new System.Drawing.Size(263, 33);
+            this.cbAttribute.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(455, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 25);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Value:";
+            // 
+            // txtValue
+            // 
+            this.txtValue.Location = new System.Drawing.Point(534, 100);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(269, 31);
+            this.txtValue.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1009, 176);
+            this.Controls.Add(this.txtValue);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbAttribute);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDir);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDir;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbAttribute;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtValue;
+    }
+}
+
---
+++ MetaDataReplacer/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MetaDataReplacer
+{
+    public partial class Form1 : Form
+    {
+        int counter;
+        int testcounter;
+        string filecnt = "";
+        string filename = @"c:\stuff\dump.txt";
+        StreamWriter sw;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            folderBrowserDialog1.RootFolder = Environment.SpecialFolder.MyComputer;
+            folderBrowserDialog1.ShowDialog();
+            txtDir.Text = folderBrowserDialog1.SelectedPath.ToString();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            if (txtDir.Text == "")
+            {
+                string message = "Please specify a directory.";
+                MessageBox.Show(message, "Specify Directory", MessageBoxButtons.OK);
+            }
+      
+
+            else
+            {
+
+                counter = 0;
+                FileInfo[] Files = AllFiles();
+                string FileCount = Files.Length.ToString();
+                filecnt = FileCount;
+                string message = "There are " + FileCount + " Files.  Press OK to start.";
+                MessageBox.Show(message, "File Number", MessageBoxButtons.OK);
+                string filepath;
+               
+
+                foreach (FileInfo testFile in Files)
+                {
+
+                    string myAttribute = cbAttribute.SelectedItem.ToString();
+                    filepath = testFile.FullName.ToString();
+                    bool myTest = TestForAttribute(filepath, myAttribute);
+
+                    if (myTest == false)
+                    {
+                        AddAttribute(filepath);        
+
+                    }
+                    else
+                    {
+                       UpdateAttribute(filepath);
+                    }
+                    
+
+                }
+
+
+            }
+
+            string endmessage = "Files found: " + filecnt + ".  Files Processed: " + counter.ToString() + ".";
+            MessageBox.Show(endmessage, "Done", MessageBoxButtons.OK);
+        }
+
+        public void UpdateAttribute(string path)
+        {
+
+            testcounter = counter;
+            var txtLines = File.ReadAllLines(path);
+            List<string> TestList = new List<string>(txtLines);
+            string attribute = cbAttribute.SelectedItem.ToString();
+
+            //const string quote = @":";
+            string replace = "";
+            string value = txtValue.Text.ToString();
+            //string author = txtAuthor.Text.ToString();
+
+            replace = attribute + " " + value;
+            //author = "ms.author: " + author;
+
+         
+
+                for (int i = 0; i < TestList.Count; i++)
+                {
+
+                    if (TestList[i].Contains(attribute))
+                    {
+
+                        TestList[i] = replace;
+                        counter = counter + 1;
+                    }
+
+
+                }
+
+            
+
+
+            txtLines = TestList.ToArray();
+            File.WriteAllLines(path, txtLines);
+            if (testcounter == counter)
+            {
+
+                string filenameandloc = TestList[2].ToString();
+
+                sw.WriteLine(path);
+
+            }
+
+        }
+
+        public FileInfo[] AllFiles()
+        {
+            DirectoryInfo dinfo = new DirectoryInfo(txtDir.Text.ToString());
+            FileInfo[] Files = dinfo.GetFiles("*.md", SearchOption.AllDirectories);
+            return Files;
+
+        }
+
+        bool TestForAttribute(string path, string attrib)
+        {
+
+            bool test = false;
+            var txtLines = File.ReadAllLines(path);
+            List<string> TestList = new List<string>(txtLines);
+            foreach (var line in TestList)   //Loop through list to find the title: line
+
+            {
+                string tmatch = line.ToString();   //if line contains title: then store this value in a string to use as the insertion point
+                tmatch = tmatch.ToLower();
+                if (tmatch.Contains(attrib))
+                {
+                    test = true;
+                    return test;
+                }
+
+            }
+
+            return test;
+
+        }
+
+        public void AddAttribute(string path)
+        {
+            string attribute = cbAttribute.SelectedItem.ToString();
+            string replace = "";
+            string value = txtValue.Text.ToString();
+            replace = attribute + " " + value;
+            
+            
+            string insertpoint = "ms.author:";
+
+        
+            var endTag = String.Format("[/{0}]", insertpoint);
+            var txtLines = File.ReadAllLines(path);
+
+            List<string> TestList = new List<string>(txtLines);
+
+            foreach (var titlefind in TestList)   //Loop through list to find the title: line
+
+            {
+
+
+                string tmatch = titlefind.ToString();   //if line contains title: then store this value in a string to use as the insertion point
+                if (tmatch.Contains("ms.author:"))
+                {
+                    insertpoint = tmatch.ToString();
+                }
+
+            }
+
+            TestList.Insert(TestList.IndexOf(insertpoint), replace);  // insert guid line into list
+            txtLines = TestList.ToArray();                               // convert list back to string[]
+
+            //txtLines.Insert(txtLines.IndexOf(insertpoint), linetoadd);
+            File.WriteAllLines(path, txtLines);
+            counter = counter + 1;
+        }
+
+
+    }
+}
